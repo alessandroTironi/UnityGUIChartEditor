021cef6f6b21266090b1824fee6e3e2d4d56b985
Thomas <thsbrown1@gmail.com>
2021-08-19 11:30:48 -0700

    Adds the ShowAltPeriodOption which will show a "." instead of a "," when added

---
+++ Assets/Plugins/Chart Editor/Scripts/ShowAltPeriodOption.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace Syrus.Plugins.ChartEditor
+{
+    public class ShowAltPeriodOption : ChartOption
+    {
+        public ShowAltPeriodOption() : base(1)
+        {
+        }
+
+        public override void ApplyOption()
+        {
+            string digitsFolder = "Chart Editor";
+            string dotTexFile = Path.Combine(digitsFolder, "Digit_Dot_Alt");
+            GUIChartEditor.sprites.Digits["."] = Resources.Load<Texture2D>(dotTexFile);
+        }
+    }
+}
